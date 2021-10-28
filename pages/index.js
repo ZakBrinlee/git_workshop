011fb51bf94151ba15f3813ccd04634cb0e2fb60
Zak Brinlee <zbrinlee@hotmail.com>
2021-10-27 19:00:03 -0700

    another title update

--- pages/index.js
+++ pages/index.js
@@ -4,7 +4,7 @@ export default function Home() {
   return (
     <div className="container">
       <Head>
-        <title>Git Workshop Wednesday</title>
+        <title>Git Workshop Wednesday in October</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
